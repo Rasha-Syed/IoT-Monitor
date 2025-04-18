a50c5f815fb06b90b33a5fb8fc6a90d4d3510d1d
Rasha-Syed <rashanishath@gmail.com>
2025-04-18 22:42:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://rashanishath:1jtE4cE9cBX2B6LC@cluster0.y499y.mongodb.net/iot", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 .then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
