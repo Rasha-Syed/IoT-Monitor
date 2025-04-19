10bc5f7e5f778e63343d4fffa9cde58b0a095bb3
Rasha-Syed <rashanishath@gmail.com>
2025-04-19 12:19:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
 const sensorRoutes = require("./routes/sensorRoutes");
+const simulatorRoutes = require("./routes/simulatorRoutes"); 
 app.use("/api/sensors", sensorRoutes);
+app.use("/api/simulator", simulatorRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
