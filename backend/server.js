1da3bc46f858dcc63612d0dbb7e22af232fc371a
Rasha-Syed <rashanishath@gmail.com>
2025-04-19 10:57:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://io-t-monitor.vercel.app/', 'https://iot-monitor.onrender.com/'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true 
-}));
+app.use(cors());
 
 app.use(express.json());
 
